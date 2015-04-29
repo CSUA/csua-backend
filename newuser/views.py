d8dbbe7f050572ccc05664315d61753312c104c7
root <root@www.www.csua.berkeley.edu>
2015-04-29 15:01:03 -0700

    [newuser] spacing fixed, etc. DO NOT LEAVE UNCOMMITTED CHANGES ON WWW

--- newuser/views.py
+++ newuser/views.py
@@ -27,19 +27,19 @@ def create(request):
       template = loader.get_template("create_failure.html")
       context = RequestContext(request, {'error':'Officer validation failed.'})
       return HttpResponse(template.render(context))
-      status, uid = ldap_bindings.NewUser(str(username), str(full_name), str(email), int(student_id), str(password))
-      print "UID:{0}".format(uid)
-      if not status:
-        template = loader.get_template("create_failure.html")
-        context = RequestContext(request, {'error':'Your username is already taken.'})
-        return HttpResponse(template.render(context))
-      mkdir("/home/{0}".format(username))
-      with open("/home/{0}/.forward".format(username),"w") as fd:
-        fd.write(email)
+    status, uid = ldap_bindings.NewUser(str(username), str(full_name), str(email), int(student_id), str(password))
+    print "UID:{0}".format(uid)
+    if not status:
+      template = loader.get_template("create_failure.html")
+      context = RequestContext(request, {'error':'Your username is already taken.'})
+      return HttpResponse(template.render(context))
+    mkdir("/home/{0}".format(username))
+    with open("/home/{0}/.forward".format(username),"w") as fd:
+      fd.write(email)
     system("ssh root@nfs chown {0} {1}".format(uid, "/nfs/homes/{0}".format(username)))
     system("ssh root@nfs chown {0} {1}".format(uid, "/nfs/homes/{0}/.forward".format(username))) 
-  #TODO(alchu): If enroll_jobs, enroll user in jobs mailing list.
-        #Temporary measure(austin): writing users to a temporary file ("jobs_mailing_list"), to be added manually
+#TODO(alchu): If enroll_jobs, enroll user in jobs mailing list.
+      #Temporary measure(austin): writing users to a temporary file ("jobs_mailing_list"), to be added manually
     if enroll_jobs:
       try:
         system("ssh root@mail echo {0} | add_members -r - Jobs".format(email))  
@@ -47,10 +47,6 @@ def create(request):
           #f.write(username + " " + email + "\n")
       except:
         print "This should never happen (mailing list file does not exist)"
-        template = loader.get_template("create_success.html")
-        context = RequestContext(request, {})
-        return HttpResponse(template.render(context))
-    else:
-      template = loader.get_template("create.html")
-      context = RequestContext(request, {})
-      return HttpResponse(template.render(context))
+    template = loader.get_template("create_success.html")
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
