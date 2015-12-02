78e90128cdb1714e0d9f6422360217de07784d5f
Caleb Wyllie <recursiveprogrammer@gmail.com>
2015-12-02 04:00:03 -0800

    Comment out duplicate entry in INSTALLED_APPS

--- csua_backend/settings.py
+++ csua_backend/settings.py
@@ -127,7 +127,7 @@ INSTALLED_APPS = (
 'django.contrib.admin',
 # Uncomment the next line to enable admin documentation:
 # 'django.contrib.admindocs',
-'django.contrib.staticfiles',
+#'django.contrib.staticfiles',
 'main_page',
 'newuser',
 'tracker',
