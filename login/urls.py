de5509a44667d8be3c31819647c1702c1454ffa9
Austin Kim <kim.austin.h@gmail.com>
2016-01-28 13:04:40 -0800

    Revert "[login] added user verification"
    This reverts commit dc29d1eebad159551e748ce9fc84ed5ae2a490da.

--- login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from newuser import views
-
-urlpatterns = [
-    url(r'^$',views.index),
-    url(r'^auth/$', views.auth),
-    url(r'^verify/$', views.verify),
-    ]
