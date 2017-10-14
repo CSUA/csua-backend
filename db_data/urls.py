42ea92e0feab312b557b12bfaeba9bdb16fe26ee
Caleb Wyllie <wylliec@users.noreply.github.com>
2017-10-15 02:43:33 +0800

    React interop (#15)
    * JSON API to the django database
    
    * change api to more intuitive name

--- db_data/urls.py
+++ db_data/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^officers/$', views.officers),
     url(r'^politburo/$', views.politburo),
     url(r'^sponsors/$', views.sponsors),
+    url(r'^api/db.json$', views.json),
     ]
