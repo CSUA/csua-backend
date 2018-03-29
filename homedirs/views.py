6d4d49fe1d94ed21d401e16f0cde1364dfafb2e1
Robert Quitt <robertquitt@gmail.com>
2018-03-29 02:26:55 -0700

    Fix MIME types for homedirs

--- homedirs/views.py
+++ homedirs/views.py
@@ -2,12 +2,16 @@
 from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 from django.shortcuts import render
+
 from hashlib import sha512
 import base64
 from json import loads, dumps
 from datetime import datetime
 from time import sleep, time
 from os import path
+
+import magic
+# Python wrapper for libmagic--gets filetype
 
 def get_resource_uri(uri):
   print(uri)
@@ -25,4 +29,5 @@ def serve(request, username = None, path = None):
   resource_uri = get_resource_uri("/home/{0}/public_html/{1}".format(username, path))
   if not resource_uri:
     raise Http404("Could not find the requested file")
-  return HttpResponse(open(resource_uri,'rb').read())
+  mime = magic.from_file(resource_uri, mime=True)
+  return HttpResponse(open(resource_uri,'rb').read(), content_type=mime)
