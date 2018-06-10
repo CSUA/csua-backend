6fc4bcaa5dc5e36599553f12c25cbaccdcbee584
Robert Quitt <robertquitt@gmail.com>
2018-06-10 14:55:00 -0700

    Make admin interface slightly nicer
    - Sort officers, making searches O(log N) instead of O(N)

--- db_data/admin.py
+++ db_data/admin.py
@@ -3,7 +3,20 @@ from django.contrib import admin
 from .models import Event, Officer, Politburo, Sponsor
 
 # Register your models here.
-admin.site.register(Officer)
-admin.site.register(Politburo)
-admin.site.register(Sponsor)
-admin.site.register(Event)
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Officer)
+class OfficerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'office_hours', 'root_staff', 'blurb')
+    ordering = ('first_name', 'last_name')
+
+@admin.register(Politburo)
+class PolituburoAdmin(admin.ModelAdmin):
+    list_display = ('position', 'title', 'officer')
+    ordering = ('position',)
+
+@admin.register(Sponsor)
+class SponsorAdmin(admin.ModelAdmin):
+    pass
