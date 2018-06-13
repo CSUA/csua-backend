ef8c6da8e29e48bbce5da3b2bfd01ef6e9063769
Robert Quitt <robertquitt@gmail.com>
2018-06-13 01:46:54 -0700

    Working LDAP connection
    Set up ldapdb to read from our ldap database. Works over internet
    too(this means you can connect to LDAP from anywhere, provided you
    havebind with a CSUA account).
    
    Because we are now working with live data, we should probably set
    upautomated backups of the database, or we may risk losing important
    data.
    
    We will also need to renew the cert at some point, we'll have to figure
    out how to automate that, too.
    
    Also have to fill in the user info to access the database.
    
    Also have to fix the admin view for the groups, it's a bit messed up.
    
    Also, ldapdb does not support migrations. We would have to make schema
    changes directly to the openldap database

--- ldap_data/models.py
+++ ldap_data/models.py
@@ -6,7 +6,7 @@ class LdapGroup(ldapdb.models.Model):
     Class for representing an LDAP group entry.
     """
     # LDAP meta-data
-    base_dn = "ou=groups,dc=csua,dc=berkeley,dc=edu"
+    base_dn = "ou=Group,dc=csua,dc=berkeley,dc=edu"
     object_classes = ['posixGroup']
 
     # posixGroup attributes
@@ -19,3 +19,26 @@ class LdapGroup(ldapdb.models.Model):
 
     def __unicode__(self):
         return self.name
+
+class LdapUser(ldapdb.models.Model):
+    """
+    Class for representing an LDAP user entry.
+    """
+    # LDAP meta-data
+    base_dn = "ou=People,dc=csua,dc=berkeley,dc=edu"
+    object_classes = ('posixAccount', 'shadowAccount')
+
+    # posixAccount
+    uid = IntegerField(db_column='uidNumber', unique=True)
+    group = IntegerField(db_column='gidNumber')
+    gecos = CharField(db_column='gecos')
+    home_directory = CharField(db_column='homeDirectory')
+    login_shell = CharField(db_column='loginShell', default='/bin/bash')
+    username = CharField(db_column='uid', primary_key=True)
+    password = CharField(db_column='userPassword')
+
+    def __str__(self):
+        return self.username
+
+    def __unicode__(self):
+        return self.full_name
