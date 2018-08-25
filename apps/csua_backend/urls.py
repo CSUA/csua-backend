5f48ad373217e3f089c0aea873e9e95b9fb47c39
Robert Quitt <robertquitt@gmail.com>
2018-08-25 15:27:45 -0700

    [fb-events] Add "fetch events" button

--- apps/csua_backend/urls.py
+++ apps/csua_backend/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     url(r'^api/v2/', include('fiber.rest_api.urls')),
     url(r'^newuser/', include('apps.newuser.urls')),
     url(r'^computers/', include('apps.tracker.urls')),
-    url(r'^api/events/', apps.fb_events.views.fetch_events),
     url(r'^~', include('apps.homedirs.urls')),
     url(r'', include('apps.db_data.urls')),
     url(r'^', include("apps.main_page.urls")),
