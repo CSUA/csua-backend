6f0841a078a55f12aa0844356424ab459f58a908
Robert Quitt <robertquitt@gmail.com>
2019-02-06 15:13:36 -0800

    Add newuser form

---
+++ apps/newuser/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from apps.ldap_data.models import LdapUser, LdapGroup
+
+usernameWhitelist = set(".-_")
+
+
+def validate_username_chars(value):
+    if not all(c.isalnum() or c in usernameWhitelist for c in value):
+        raise ValidationError(
+            "Username must consist of alphanumeric characters and ., -, or _"
+        )
+
+
+def validate_username_in_use(value):
+    if LdapUser.objects.filter(username=value).exists():
+        raise ValidationError("Username already taken")
+    if LdapGroup.objects.filter(name=value).exists():
+        raise ValidationError("Username already taken")
+
+
+class NewUserForm(forms.Form):
+    full_name = forms.CharField(label="Full Name")
+    student_id = forms.IntegerField(
+        label="Student ID", min_value=100000, max_value=10000000000
+    )
+    email = forms.EmailField()
+    username = forms.CharField(
+        validators=[validate_username_chars, validate_username_in_use]
+    )
+    password = forms.CharField(widget=forms.PasswordInput())
+    enroll_jobs = forms.BooleanField(required=False)
+    officer_username = forms.CharField(label="Officer Username")
+    officer_password = forms.CharField(
+        widget=forms.PasswordInput(), label="Officer Password"
+    )
+    agree_rules = forms.BooleanField(required=True)
