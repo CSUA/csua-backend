215f6afa2ecfd74561c289eaf4fb087d9ee708bc
Robert Quitt <robertquitt@gmail.com>
2019-04-22 03:18:43 -0700

    Clean up newuser
    Make it look gooder and have correct error handling

--- apps/newuser/ldap_bindings.py
+++ apps/newuser/ldap_bindings.py
@@ -1,110 +1,82 @@
 import hashlib
 import string
 from base64 import b64encode
-from os import popen
 from random import choice
 
 from ldap3 import ALL, ALL_ATTRIBUTES, Connection, Server
-
-LDAP_SERVER = "ldaps://ldap.csua.berkeley.edu"
-LDAP_USER = "uid=newuser,ou=People,dc=csua,dc=berkeley,dc=edu"
-LDAP_PASSWORD = ""
+from decouple import config
 
 
-def GetLdapPassword():
-    global LDAP_PASSWORD
-    if LDAP_PASSWORD:
-        return LDAP_PASSWORD
-    try:
-        with open("/etc/secrets/newuser.secret") as f:
-            LDAP_PASSWORD = f.read().strip()
-    except FileNotFoundError:
-        LDAP_PASSWORD = ""
-    return LDAP_PASSWORD
+LDAP_SERVER_URL = "ldaps://ldap.csua.berkeley.edu"
+NEWUSER_DN = "uid=newuser,ou=People,dc=csua,dc=berkeley,dc=edu"
+NEWUSER_PW = config("NEWUSER_PW")
+
+def get_max_uid():
+    with Connection(LDAP_SERVER_URL) as conn:
+        conn.search("ou=People,dc=csua,dc=berkeley,dc=edu", "(cn=*)", attributes="uidNumber")
+        max_uid = max(int(str(entry.uidNumber)) for entry in conn.entries)
+        return max_uid
 
 
-def GetMaxUID():
-    return (
-        int(
-            popen(
-                "ldapsearch -x 'UIDNumber' | grep uidNumber | awk '{print $2}' | sort -n | tail -n 1"
-            ).read()
-        )
-        + 1
-    )
-
-
-def MakePassword(password):
+def make_password(password):
+    """
+    generates a salted SHA-1 hash of the given password
+    """
     salt = "".join(choice(string.ascii_letters + string.digits) for _ in range(4))
     ctx = hashlib.sha1(password.encode("utf-8"))
     ctx.update(salt.encode("utf-8"))
     return "{SSHA}" + b64encode(ctx.digest() + salt.encode("utf-8")).decode("utf-8")
 
 
-def NewUser(username, name, email, sid, password):
-    s = Server(LDAP_SERVER, get_info=ALL)
-    uid = -1
-    c = Connection(s, user=LDAP_USER, password=GetLdapPassword())
-    if c.bind():
-        dn = "uid={0},ou=people,dc=csua,dc=berkeley,dc=edu".format(username)
-        uid = GetMaxUID()
-        attrs = {
-            "uid": username,
-            "objectclass": ["account", "posixaccount", "top", "shadowaccount"],
-            "homedirectory": "/home/{0}".format(username),
-            "uidnumber": str(uid),
-            "shadowmax": "99999",
-            "gidnumber": "1000",
-            "cn": username,
-            "shadowwarning": "7",
-            "sid": str(sid),
-            "userpassword": MakePassword(password),
-            "gecos": "{0},{1}".format(name, email),
-            "loginshell": "/bin/bash",
-        }
-        c.add(dn, attributes=attrs)
-        c.unbind()
-        return True, uid
-    c.unbind()
-    return False, uid
+def create_new_user(username, name, email, sid, password):
+    """
+    binds as newuser and creates a new user. be careful that this isn't called by any unpriveleged views.
+    """
+    with Connection(LDAP_SERVER_URL, user=NEWUSER_DN, password=NEWUSER_PW) as c:
+        if c.bind():
+            dn = "uid={0},ou=people,dc=csua,dc=berkeley,dc=edu".format(username)
+            uid = get_max_uid()
+            attrs = {
+                "uid": username,
+                "objectclass": ["account", "posixaccount", "top", "shadowaccount"],
+                "homedirectory": "/home/{0}".format(username),
+                "uidnumber": str(uid),
+                "shadowmax": "99999",
+                "gidnumber": "1000",
+                "cn": username,
+                "shadowwarning": "7",
+                "sid": str(sid),
+                "userpassword": make_password(password),
+                "gecos": "{0},{1}".format(name, email),
+                "loginshell": "/bin/bash",
+            }
+            success = c.add(dn, attributes=attrs)
+            return success, uid
+        else:
+            return False, -1
 
 
-def DeleteUser(username):  # never used
-    s = Server(LDAP_SERVER, get_info=ALL)
-    c = Connection(s, user=LDAP_USER, password=GetLdapPassword())
-    deleteDN = "uid={0},ou=People,dc=csua,dc=berkeley,dc=edu".format(username)
-    if c.bind():
-        c.delete(deleteDN)
-    c.unbind()
+def authenticate(username, password):
+    """
+    verifies that the username and password are correct
+    """
+    user_dn = "uid={0},ou=People,dc=csua,dc=berkeley,dc=edu".format(username)
+    with Connection(LDAP_SERVER_URL, user=user_dn, password=password) as c:
+        if c.bind():
+            return True
+        else:
+            return False
 
 
-def Authenticate(username, password):
-    user_dn = "uid={0},ou=people,dc=csua,dc=berkeley,dc=edu".format(username)
+def is_officer(username):
     base_dn = "dc=csua,dc=berkeley,dc=edu"
-    s = Server(LDAP_SERVER, get_info=ALL)
-    search_filter = "(uid=" + username + ")"
-    c = Connection(s, user=user_dn, password=password)
-    if c.bind():
-        c.search(base_dn, search_filter)
-        c.unbind()
-        return True
-    c.unbind()
-    return False
-
-
-def IsOfficer(username):
-    base_dn = "dc=csua,dc=berkeley,dc=edu"
-    s = Server(LDAP_SERVER, get_info=ALL)
+    s = Server(LDAP_SERVER_URL)
     search_filter = "(cn=officers)"
-    c = Connection(s, user=LDAP_USER, password=GetLdapPassword())
-    if c.bind():
+    with Connection(s) as c:
         c.search(base_dn, search_filter, attributes=ALL_ATTRIBUTES)
-        result = c.response
-        c.unbind()
-        officers = result[0]["attributes"]["memberUid"]
+        officers = c.entries[0].memberUid
         return username in officers
-    c.unbind()
 
 
-def ValidateOfficer(username, password):
-    return IsOfficer(username) and Authenticate(username, password)
+def validate_officer(username, password):
+    return is_officer(username) and authenticate(username, password)
