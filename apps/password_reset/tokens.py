664b5cceede7b05b89e8df8aba02140a3dcb129d
Bassam Safadi <samsafadi@gmail.com>
2020-07-19 19:07:57 -0700

    added a couple of imports

--- apps/password_reset/tokens.py
+++ apps/password_reset/tokens.py
@@ -1,3 +1,5 @@
+from django.utils.crypto import salted_hmac
+from django.utils.http import int_to_base36
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import six
 
@@ -5,9 +7,11 @@ from apps.ldap.utils import get_user_hashed_password
 
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
+
         # This uses the old hashed password and timestamp as a hash value
         # so that 1. you manage the time and 2. when you change the password
         # it becomes invalidated
+
         return (
             six.text_type(get_user_hashed_password(user)) + six.text_type(timestamp)
         )
