57ed584843b3451b6a7257dd963bd28883d8fb80
Bassam Safadi <samsafadi@gmail.com>
2020-07-22 21:08:04 -0700

    got it to work up until the POST from the reset-password-confirm which is returning a 405 method not allowed

--- apps/password_reset/urls.py
+++ apps/password_reset/urls.py
@@ -4,11 +4,11 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from . import views
 
 urlpatterns = [
+    path(
+        "reset-password-confirm/<uid>_<token>",
+        views.PasswordResetView.as_view(),
+        name="reset-password-confirm",
+    ),
     path("", views.RequestPasswordResetView, name="request-reset-password"),
     path("token/", views.RequestPasswordResetView, name="reset-password-token"),
-    path(
-        "activate_account/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
-        views.PasswordResetView.as_view(),
-        name="reset-password",
-    ),
 ]
