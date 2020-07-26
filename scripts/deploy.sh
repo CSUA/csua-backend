0858f7fcd4648d43a616cdc37ab6c40ff16325eb
Robert Quitt <robertquitt@gmail.com>
2020-07-26 03:43:04 -0700

    Test #3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,8 @@
-HERE=$(dirname $0)
-KEYFILE=$HERE/id_rsa
 BRANCH=master
-stat $KEYFILE
 ssh \
     -o StrictHostKeyChecking=no \
     www-data@tap.csua.berkeley.edu \
-    -i $KEYFILE '
-	cd /webserver/csua-backend \
+    'cd /webserver/csua-backend \
 	&& git pull origin $BRANCH \
 	&& venv/bin/python manage.py test --keepdb --noinput \
 	&& venv/bin/python manage.py migrate --noinput \
