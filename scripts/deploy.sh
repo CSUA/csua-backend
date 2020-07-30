12b8ffbd1d30e2f75dab4a36956ba5e567d52aea
Robert Quitt <robertquitt@gmail.com>
2020-07-30 16:06:31 -0700

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@ ssh \
     www-data@tap.csua.berkeley.edu \
     'cd /webserver/csua-backend \
 	&& git pull origin $BRANCH \
+	&& venv/bin/pip install -r requirements.txt \
 	&& venv/bin/python manage.py test --keepdb --noinput \
 	&& venv/bin/python manage.py migrate --noinput \
 	&& venv/bin/python manage.py collectstatic --noinput \
