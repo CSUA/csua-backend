a882f54e3eb9358371e78b0b747d7de17bed50af
Robert Quitt <robertquitt@gmail.com>
2020-12-10 05:48:49 -0800

    Pass tests, improvements to slackbot

--- apps/slackbot/client.py
+++ apps/slackbot/client.py
@@ -8,40 +8,32 @@ from django.conf import settings
 from decouple import config
 from slack_bolt import App
 
-SLACK_CLIENT_ID = "3311748471.437459179046"
+
+username_regexp = re.compile(r"[A-Za-z0-9.\-_]+")
+
+IS_PROD = socket.gethostname() == "tap"
+
 SLACK_BOT_USER_TOKEN = config("SLACK_BOT_USER_TOKEN", default="")
 SLACK_SIGNING_SECRET = config("SLACK_SIGNING_SECRET", default="")
 
-app = App(token=SLACK_BOT_USER_TOKEN, signing_secret=SLACK_SIGNING_SECRET)
 
-
-@app.message("based")
-def based(message, say):
+def message_based(message, say):
     for reacc in ["b", "a", "five", "e-mail", "id"]:
         app.client.reactions_add(
             channel=message["channel"], timestamp=message["ts"], name=reacc
         )
 
 
-@app.event("message")
-def message(event, say):
+def event_message(event, say):
     pass
 
 
-@app.command("/export")
-def export(ack, say, command):
+def command_export(ack, say, command):
     ack("gotcha")
 
 
-@app.command("/help")
-def help(ack, say, command):
+def command_help(ack, say, command):
     ack("Help, I'm trapped in a chatbot factory!", response_type="in_channel")
-
-
-username_regexp = re.compile(r"[A-Za-z0-9.\-_]+")
-
-
-IS_PROD = socket.gethostname() == "tap"
 
 
 def run_shell_command(command):
@@ -50,8 +42,7 @@ def run_shell_command(command):
     return subprocess.check_output(command, shell=True).decode()
 
 
-@app.command("/philfinger")
-def finger(ack, say, command):
+def command_finger(ack, say, command):
     args = command.get("text", "")
     if len(args.split()) != 1:
         ack("Usage: /philfinger <user>")
@@ -61,7 +52,7 @@ def finger(ack, say, command):
         return
     csua_user = args.strip()
     ack(f"P:hilfinger:ing {csua_user}...")
-    slack_user_info = webclient.users_info(user=command["user_id"])
+    slack_user_info = app.client.users_info(user=command["user_id"])
     print(slack_user_info)
     if not slack_user_info["ok"]:
         slack_user_info = {"user": {"profile": {"display_name": "pnunez"}}}
@@ -77,11 +68,10 @@ def finger(ack, say, command):
             response_type="in_channel",
         )
     else:
-        say(f"{slack_user_name} tried to finger {csua_user} but failed!")
+        say(f"{slack_name} tried to finger {csua_user} but failed!")
 
 
-@app.command("/man")
-def man(ack, say, command):
+def command_man(ack, say, command):
     command_text = command["text"]
     command_text_clean = shlex.quote(command_text)
     ack("Fetching man page...")
@@ -97,3 +87,15 @@ def man(ack, say, command):
     else:
         text = "Couldn't find man page. Got: {}.".format(man_proc.stderr.read())
     say(text, response_type="in_channel")
+
+
+if SLACK_BOT_USER_TOKEN and SLACK_SIGNING_SECRET:
+    app = App(token=SLACK_BOT_USER_TOKEN, signing_secret=SLACK_SIGNING_SECRET)
+    app.message("based")(message_based)
+    app.event("message")(event_message)
+    app.command("/man")(command_man)
+    app.command("/philfinger")(command_finger)
+    app.command("/export")(command_export)
+    app.command("/help")(command_help)
+else:
+    app = None
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ ssh \
     www-data@tap.csua.berkeley.edu \
     'cd /webserver/csua-backend \
 	&& git pull origin $BRANCH \
-	&& venv/bin/pip install -r requirements.txt \
+	&& venv/bin/pip install -r requirements.txt --upgrade \
 	&& venv/bin/python manage.py test --keepdb --noinput \
 	&& venv/bin/python manage.py migrate --noinput \
 	&& venv/bin/python manage.py collectstatic --noinput \
