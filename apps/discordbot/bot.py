3affc33618fe51971e9a7b9b1eaa0980eeae2dfe
Ani Nrusimha <aninrusimha@berkeley.edu>
2020-12-14 16:41:29 -0800

    fix formatting

--- apps/discordbot/bot.py
+++ apps/discordbot/bot.py
@@ -15,10 +15,10 @@ intents = discord.Intents.all()
 intents.presences = False
 
 TOKEN = config("DISCORD_TOKEN", default="")
-CSUA_GUILD_ID = int(config("TEST_GUILD", default=368282532757897217))
-CSUA_PHILBOT_CLIENT_ID = int(config("BOT_ID", default=737930184837300274))
-HOSER_ROLE_ID = int(config("TEST_ROLE", default=785418569412116513)) #Verified
-DEBUG_CHANNEL_ID = int(config("DEBUG_CHANNEL", default=787853294474100756))
+CSUA_GUILD_ID = config("TEST_GUILD", default=368282532757897217, cast=int)
+CSUA_PHILBOT_CLIENT_ID = config("BOT_ID", default=737930184837300274, cast=int)
+HOSER_ROLE_ID = config("TEST_ROLE", default=785418569412116513, cast=int)  # Verified
+DEBUG_CHANNEL_ID = config("DEBUG_CHANNEL", default=787853294474100756, cast=int)
 
 TIMEOUT_SECS = 10
 
@@ -26,39 +26,46 @@ logger = logging.getLogger(__name__)
 
 
 class CSUAClient(discord.Client):
-
     async def on_ready(self):
         print(f"{self.user} has connected to Discord")
         self.is_phillip = self.user.id == CSUA_PHILBOT_CLIENT_ID
         if self.is_phillip:
             print("Phillip is in the Office")
             self.csua_guild = get(self.guilds, id=CSUA_GUILD_ID)
-            # self.test_guild = get(self.guilds, id=TEST_GUILD_ID)
             self.test_channel = get(self.csua_guild.channels, id=DEBUG_CHANNEL_ID)
             self.hoser_role = get(self.csua_guild.roles, id=HOSER_ROLE_ID)
 
-    async def verify_member_email(self, user): 
+    async def verify_member_email(self, user):
         channel = user.dm_channel
+
         def check_msg(msg):
             return msg.channel == channel
+
         got_email = False
         while not got_email:
-            msg = await self.wait_for('message', check=check_msg)
+            msg = await self.wait_for("message", check=check_msg)
             try:
                 validate_email(msg.content)
                 if "@berkeley.edu" in msg.content:
-                    got_email= True
-                    await channel.send(f"Sending a an email to verify {user.name} to {msg.content}")
+                    got_email = True
+                    await channel.send(
+                        f"Sending a an email to verify {user.name} to {msg.content}"
+                    )
                     send_verify_mail(msg.content, user.name)
                 else:
-                    await channel.send(f"{msg.content} is not a berkeley email. Please fix this")
+                    await channel.send(
+                        f"{msg.content} is not a berkeley email. Please fix this"
+                    )
             except ValidationError as e:
-                await channel.send(f"{msg.content} is not a valid email. Please try again. Details: ", e)
-    
+                await channel.send(
+                    f"{msg.content} is not a valid email. Please try again. Details: ",
+                    e,
+                )
+
     async def on_message(self, message):
         if message.author == self.user:
             return
-        #Reading rules and verification
+        # Reading rules and verification
         msg = message.content.lower()
         if "hkn" in msg.lower() and "ieee" in msg.lower():
             await message.channel.send("Do I need to retrieve the stick?")
@@ -72,15 +79,17 @@ class CSUAClient(discord.Client):
 
     async def on_member_join(self, member):
         msg = await member.send(
-            "Welcome to the CSUA discord server! First, read the rules in #landing-zone. Thumbs up this message if you agree")
+            "Welcome to the CSUA discord server! First, read the rules in #landing-zone. Thumbs up this message if you agree"
+        )
+
         def check_thumb(react, _):
-            return react.message ==  msg and str(react.emoji) == '👍' # thumbs
-        await self.wait_for('reaction_add', check=check_thumb)
+            return react.message == msg and str(react.emoji) == "👍"  # thumbs
+
+        await self.wait_for("reaction_add", check=check_thumb)
         await member.send(
             "Verify your berkeley.edu email to gain access. First, pleast type your email. Please contact a moderator if you have any issues."
         )
 
-        # url = "https://www.csua.berkeley.edu" + reverse("discord_register")
         await self.verify_member_email(member)
         if self.is_phillip:
             await self.test_channel.send(
@@ -122,8 +131,8 @@ class CSUABot:
             return True
         return False
 
+
 if TOKEN:
     csua_bot = CSUABot()
 else:
     csua_bot = None
-
