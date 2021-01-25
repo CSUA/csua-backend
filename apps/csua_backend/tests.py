c1ebf3f7f3c21ebf29baca7432d79fea25c5b483
Robert Quitt <robertquitt@gmail.com>
2021-01-24 23:27:47 -0800

    Redirects with query parameters no longer fail

---
+++ apps/csua_backend/tests.py
@@ -0,0 +1,27 @@
+from django.test import TestCase, Client
+from django.contrib.redirects.models import Redirect
+from django.contrib.sites.models import Site
+
+
+class TestRedirectMiddleware(TestCase):
+    OLD_PATH = "/facebook/"
+    NEW_PATH = "https://facebook.com/csua/"
+
+    def setUp(self):
+        Redirect.objects.create(
+            old_path=self.OLD_PATH,
+            new_path=self.NEW_PATH,
+            site=Site.objects.get_current(),
+        )
+
+    def test_redirects(self):
+        self.makeSureItWorks("/facebook")
+        self.makeSureItWorks("/facebook/")
+        self.makeSureItWorks("/facebook?fbclid=poopy")
+        self.makeSureItWorks("/facebook/?fbclid=stinky")
+
+    def makeSureItWorks(self, url):
+        response = self.client.get(url)
+        self.assertRedirects(
+            response, self.NEW_PATH, status_code=307, fetch_redirect_response=False
+        )
