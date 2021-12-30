c1503d8ac3a6bd485f6e53ef325cc482d956cd6c
Robert Quitt <robertquitt@gmail.com>
2021-12-30 15:02:16 -0800

    Fix get_upcoming_events to not use naive datetime objects

--- apps/db_data/templatetags/db_data_tags.py
+++ apps/db_data/templatetags/db_data_tags.py
@@ -1,6 +1,5 @@
-import datetime
-
 from django import template
+from django.utils import timezone
 
 from apps.db_data.models import Event, Notice
 
@@ -9,9 +8,9 @@ register = template.Library()
 
 @register.simple_tag
 def get_upcoming_events():
-    return Event.objects.filter(start_time__gte=datetime.datetime.now())
+    return Event.objects.filter(start_time__gte=timezone.now())
 
 
 @register.simple_tag
 def get_current_notices():
-    return Notice.objects.filter(expires__gte=datetime.date.today())
+    return Notice.objects.filter(expires__gte=timezone.now().date())
