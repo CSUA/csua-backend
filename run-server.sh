f9ee09dc2ae513d32a0d4185b7e8857a830699d1
Robert Quitt <robertquitt@gmail.com>
2021-12-30 11:11:31 -0800

    Update scripts to work with soda
    - Replaces virtualenv with venv for Python-3.8 compatibility in bootstrap.sh
    - Adds option to start socat to create a proxy for easier connection to soda

--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+
+while getopts "u" opt; do
+	case "$opt" in
+		u)  unix_socket=1
+			;;
+	esac
+done
 repo_dir=$(dirname $0)
 python="${repo_dir}"/venv/bin/python3
-"${python}" manage.py runserver
+port=8000
+
+# For soda web hosting
+if [ "$unix_socket" = "1" ]; then
+	socket=$HOME/public_html/csua-backend.sock
+	read LOWERPORT UPPERPORT < /proc/sys/net/ipv4/ip_local_port_range
+	port="`shuf -i $LOWERPORT-$UPPERPORT -n 1`"
+	rm -vf $socket
+	socat UNIX-LISTEN:$socket,fork,mode=777 TCP:localhost:$port &
+	echo "Listening on http://csua-backend.$USER.soda.csua.berkeley.edu"
+else
+	if [ $HOSTNAME = "soda" ]; then
+		echo "For easier use on soda consider using $0 -u"
+	fi
+fi
+
+"${python}" manage.py runserver localhost:$port
