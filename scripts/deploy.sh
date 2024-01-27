2f494673d1384886d385b5807229ff8c0b03aca9
Ethan Zhang <36081769+e-zhang09@users.noreply.github.com>
2024-01-26 21:24:06 -0800

    feat: add actions deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,8 @@
-BRANCH=master
-ssh \
-    -o StrictHostKeyChecking=no \
-    www-data@tap.csua.berkeley.edu \
-    'cd /webserver/csua-backend \
+cd /webserver/csua-backend \
 	&& git pull origin $BRANCH \
 	&& venv/bin/pip install -r requirements.txt --upgrade \
 	&& venv/bin/python manage.py test --keepdb --noinput \
 	&& venv/bin/python manage.py migrate --noinput \
 	&& venv/bin/python manage.py collectstatic --noinput \
 	&& sudo systemctl restart csua-backend-gunicorn \
-	&& echo done'
+	&& echo done
