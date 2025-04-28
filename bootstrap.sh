c49ffe8fd71fe40c99b9e73fdb02ed41a83c97df
Blake Haug <blake@blakehaug.com>
2025-04-27 21:38:48 -0700

    Update bootstrap.sh to support /bin/sh execution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
-repo_dir=$(dirname $0)
-python="${repo_dir}"/venv/bin/python3
-pre_commit="${repo_dir}"/venv/bin/pre-commit
 
 python3 -m venv venv
-source venv/bin/activate
-pip install pipx
-pipx install poetry
+. venv/bin/activate
+pip install -U pip setuptools
+pip install poetry
 poetry install
 
 if [ ! -e "${repo_dir}/.env" ]; then
     cp "${repo_dir}/.env.dev" "${repo_dir}/.env"
 fi
 python3 manage.py migrate
-./venv/bin/pre-commit install
+pre-commit install
